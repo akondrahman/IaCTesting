9a9898f181b49693c8d9d60121ec616937c3844e
talismanic <u.urs.888@gmail.com>
2020-12-02 00:06:04 +0600

    runner update

--- runner.py
+++ runner.py
@@ -8,6 +8,9 @@ Created on Tue Dec  1 01:27:46 2020
 import main
 
 import pymysql
+import logging
+logging.basicConfig(level=logging.DEBUG,filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s)')
+
 
 db_conn = pymysql.connect("localhost","root","","test", charset='utf8' )
 cursor = db_conn.cursor()
@@ -16,7 +19,7 @@ cursor = db_conn.cursor()
 def get_all_github_repo_id():
     
     try:
-        cursor.execute('select * from final_repos ')
+        cursor.execute('select * from final_repos where id =2937')
         rows = cursor.fetchall()
     except:
         print(Exception)
@@ -31,5 +34,11 @@ for repo in repos:
     project_dir = repo[1].split("/")[1]
     full_dir = base_dir+"\\"+top_dir+"\\"+project_dir
     project_name = top_dir+"_"+project_dir
-    main.main(full_dir, project_dir)
-    
+    try:
+        main.main(full_dir, project_name)
+        logging.debug("repoid %s done", repo[0],)
+
+    except:
+        logging.error("repoid %s failed", repo[0],)
+
+        continue
